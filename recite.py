6b718ae856a3e7f5b9d0fe726728655f8e7589e8
Yu-Shiang Lin <coldfunction@gmail.com>
2018-08-09 16:32:14 +0800

    feat: Level movement

--- recite.py
+++ recite.py
@@ -10,8 +10,11 @@ def append_lines(lines, myfile):
 	f.close()
 
 def insert_lines(lines, pos, myfile):
+	if not lines:
+		f = open(myfile, "w")
+		f.close()
 	for line in lines:
-		insert_line(0, line, myfile)	
+		insert_line(0, line, myfile)
 
 def insert_line(index, line, myfile):
 	f = open(myfile, "r")
@@ -30,6 +33,29 @@ def recite_voc():
 	numOflines = len(lines)
 	fp.close()
 	
+def move_level(src, dst, num):
+	fp = open(src, "r")
+	lines = fp.readlines()
+	length = len(lines)
+	move_lines = []
+
+	for i in range(length-1, length-11, -1):
+		if i < 0:
+			break
+		move_lines.append(lines[i])
+		print(lines[i])
+
+	length = len(move_lines)
+	fp.close()
+
+	fp = open(src, "w")
+	for i in range(0, length-10):
+		fp.write(lines[i])
+	fp.close()
+	
+	append_lines(move_lines, dst)
+
+	
 
 def recite_voc():
 	fp = open("1_level.txt", "r")
@@ -40,9 +66,14 @@ def recite_voc():
 
 	temp_lines = []
 	move_lines = []
+
+	if numOflines == 0 :
+		#from, to, nums
+		move_level("2_level.txt", "1_level.txt", 10)
+		recite_voc()
+
 	for line in lines:
 		os.system('clear')
-		print("numof lines ", numOflines)
 		myvoc = line.split("@")
 		print(myvoc[0])
 		input('answer: \n')
